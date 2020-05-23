721191684aeb39c6795ba951e537481613cc041c
Tamyres Luiz Machado Neto <tamyres.neto@outlook.com>
2020-05-23 20:11:12 -0300

    Add project files.

---
+++ IdentityServerWithMvc/Config.cs
@@ -0,0 +1,97 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServerWithMvc
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                // OpenID Connect implicit flow client (MVC)
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+
+                    RedirectUris = { "http://localhost:5001/signin-oidc"  },
+                    PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc" },
+                    ClientSecrets = {new Secret("super-secret".ToSha256(),"mvc-secret") },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Address,
+                        "website"
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "bruno",
+                    Password = "1234",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.Name, "Bruno"),
+                        new Claim(JwtClaimTypes.GivenName, "Bruno Brito"),
+                        new Claim(JwtClaimTypes.FamilyName, "Brito"),
+                        new Claim(JwtClaimTypes.Email, "bhdebrito@gmail.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://www.saindodacaixinha.com.br"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'Av Paulista', 'locality': 'Sao Paulo', 'postal_code': 0332303, 'country': 'Brazil' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "jp",
+                    Password = "1234",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.Name, "JP"),
+                        new Claim(JwtClaimTypes.GivenName, "Joao Pedro"),
+                        new Claim(JwtClaimTypes.FamilyName, "Silva"),
+                        new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://globo.com"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'Rua da Consolacao', 'locality': 'Sao Paulo', 'postal_code': 02565333, 'country': 'Brasil' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json),
+                        new Claim("location", "Brazil")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServerWithMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IdentityServerWithMvc.Models;
+
+namespace IdentityServerWithMvc.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        private readonly ILogger<Home1Controller> _logger;
+
+        public Home1Controller(ILogger<Home1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
